47bc6a99095ed4fc8676cb5ce0d3bbb907b48201
Hugo_Ribeiro <53909859+Lokiatos@users.noreply.github.com>
2020-05-07 10:59:21 -0300

    Create generator 0.0.1.py

---
+++ Gerador de CNPJ/previous_version/generator 0.0.1.py
@@ -0,0 +1,35 @@
+def generate():
+    from random import randint
+    while True:
+        actual_cnpj = str(randint(10000000, 99999999))
+        if actual_cnpj[0] * 8 == actual_cnpj:
+            continue
+        else:
+            break
+    actual_cnpj += '0001'
+    with1digit = actual_cnpj + str(calculate_1stdigit(actual_cnpj))
+    with2digits = with1digit + str(calculate_2stdigit(with1digit))
+    return (with2digits)
+
+
+
+def calculate_1stdigit(clean_cnpj):
+    sequence = '678923456789'
+    total = 0
+    for count in range(0, 12):
+        total += int(clean_cnpj[count]) * int(sequence[count])
+    first_digit = (total % 11)
+    if first_digit > 9:
+        first_digit = 0
+    return first_digit
+
+
+def calculate_2stdigit(cnpj_with1digit):
+    sequence2 = '5678923456789'
+    total = 0
+    for count in range(0, 13):
+        total += (int(cnpj_with1digit[count]) * (int(sequence2[count])))
+    second_digit = (total % 11)
+    if second_digit > 9:
+        second_digit = 0
+    return second_digit
