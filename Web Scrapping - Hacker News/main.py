12aa5bb894db09f8dd88b1f4c6c2959ecd4577db
Hugo_Ribeiro <53909859+Lokiatos@users.noreply.github.com>
2020-06-01 17:04:11 -0300

    Web Scrapping and more

---
+++ Web Scrapping - Hacker News/main.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+import pprint
+
+
+def loop_pages():
+    total_list = []
+    pages = int(input('Tell me how many pages do you wanna I check: '))
+    for page in range(1, pages+1):
+        res = requests.get(f'https://news.ycombinator.com/news?p={page}')
+        soup = BeautifulSoup(res.text, 'html.parser')
+        links = (soup.select('.storylink'))
+        subtext = soup.select('.subtext')
+        actual_list = (create_custom_hn(links, subtext))
+        total_list.extend(actual_list)
+    total_list = sort_stories_by_votes(total_list)
+    pprint.pprint(total_list)
+
+
+def sort_stories_by_votes(hn_list):
+    return sorted(hn_list, key=lambda k: k['votes'], reverse=True)
+
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for index, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[index].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return sort_stories_by_votes(hn)
+
+
+loop_pages()
