2f0516e0181fc4c4517eff56e17df89569c65245
Hugo Ribeiro <hugolerib@gmail.com>
2020-07-19 11:23:48 -0300

    Guess the Number - Added try/except and adjust the call of functions

--- Guess the Number.py
+++ Guess the Number.py
@@ -1,6 +1,11 @@
 # 2. Guess the Number
 
-# The Goal: Similar to the first project, this project also uses the random module in Python. The program will first randomly generate a number unknown to the user. The user needs to guess what that number is. (In other words, the user needs to be able to input information.) If the user’s guess is wrong, the program should return some sort of indication as to how wrong (e.g. The number is too high or too low). If the user guesses correctly, a positive indication should appear. You’ll need functions to check if the user input is an actual number, to see the difference between the inputted number and the randomly generated numbers, and to then compare the numbers.
+# The Goal: Similar to the first project, this project also uses the random module in Python. The program will first
+# randomly generate a number unknown to the user. The user needs to guess what that number is. (In other words,
+# the user needs to be able to input information.) If the user’s guess is wrong, the program should return some sort
+# of indication as to how wrong (e.g. The number is too high or too low). If the user guesses correctly, a positive
+# indication should appear. You’ll need functions to check if the user input is an actual number, to see the
+# difference between the inputted number and the randomly generated numbers, and to then compare the numbers.
 # Concepts to keep in mind:
 
 # Random function
@@ -11,11 +16,17 @@
 # While loops
 # If/Else statements
 
+# Programmer: Hugo Leça Ribeiro
+
 import random
 
 
 def verify(number_rand):
-    number_player = int(input("\nTry to guess. The number is between 0 and 100: "))
+    try:
+        number_player = int(input("\nTry to guess. The number is between 0 and 100: "))
+    except ValueError:
+        print(f'Wrong value, try again please!')
+        verify(number_rand)
     if number_player == number_rand:
         print('Heyyy, congratulations! You got the number right!')
         return
@@ -27,18 +38,18 @@ def verify(number_rand):
 
 def difference(number_player, number_rand):
     if number_player > number_rand:
-        difference = number_player - number_rand
-        if difference > 10 and difference <= 25:
+        dif = number_player - number_rand
+        if 10 < dif <= 25:
             print('Owww, you are far from the number. The number is lowest than this.')
-        elif difference > 25:
+        elif dif > 25:
             print('WOW, you are very far from the number. The number is lowest than this.')
         else:
             print('Hmmm, you are so close. The number is lowest than this.')
     elif number_rand > number_player:
-        difference = number_rand - number_player
-        if difference > 10 and difference <= 25:
+        dif = number_rand - number_player
+        if 10 < dif <= 25:
             print('Owww, you are far from the number. The number is highest than this.')
-        elif difference > 25:
+        elif dif > 25:
             print('WOW, you are very far from the number. The number is highest than this.')
         else:
             print('Hmmm, you are so close. The number is highest than this.')
@@ -48,7 +59,8 @@ def difference(number_player, number_rand):
 def generate_number():
     print('Hello, player. Try to found the number that i am thinking right now. I doubt!')
     number_rand = random.randint(0, 100)
-    verify(number_rand)
+    return number_rand
 
 
-generate_number()
+generated_number = generate_number()
+verify(generated_number)
