4d6a2bc35701511d5d8e9849ba8f166a699ff0fb
Hugo <hugolerib@gmail.com>
2021-05-30 10:45:47 -0300

    Pratica 1 subprocess

--- praticas_SOII_Fatec/ModuloSubprocess/Pratica1/main.py
+++ praticas_SOII_Fatec/ModuloSubprocess/Pratica1/main.py
@@ -1,3 +1,12 @@
 import subprocess
 
+subprocess.call(['ls', '-lah'])
+
+print('__________________________________\n')
 subprocess.run(["touch", "testando.txt"])
+subprocess.call(['ls', '-lah'])
+
+print('-----------------------------------\n')
+result = subprocess.check_output(['ls', '-lah'])
+# Transforma de bytes para string
+print(result.decode())
