ac46c999c67f6432ac66820be81888cf4e1adaee
Hugo Ribeiro <53909859+hugolribeiro@users.noreply.github.com>
2021-06-03 11:42:17 -0300

    Update main.py

--- praticas_SOII_Fatec/ImportandoModulos/Pratica5/main.py
+++ praticas_SOII_Fatec/ImportandoModulos/Pratica5/main.py
@@ -1,6 +1,17 @@
-
 # Criando arquivo 1 com 10 linhas
 with open('arquivo1.txt', 'w') as arq1:
-    for line in range(1, 10):
+    for line in range(1, 11):
         arq1.write(f'Linha {line} do arquivo 1\n')
-    arq1.write(f'Linha {line+1} do arquivo 1')
+
+# Criando arquivo 2 com 10 linhas
+with open('arquivo2.txt', 'w') as arq1:
+    for line in range(1, 11):
+        arq1.write(f'Linha {line} do arquivo 2\n')
+
+# Lendo ambos arquivos e intercalando
+with open('arquivo1.txt', 'r') as arq1:
+    with open('arquivo2.txt', 'r') as arq2:
+        with open('arquivo3.txt', 'w') as arq3:
+            for line in range(0, 20):
+                arq3.write(arq1.readline())
+                arq3.write(arq2.readline())
